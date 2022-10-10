f9f235c660810f52feb73d2b5f9380f2335bb60a
AZZ-vopp <64297334+AZZ-vopp@users.noreply.github.com>
2022-10-10 18:56:20 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,25 +1,35 @@
-clear
-echo "   1. Install"
-echo "   2. Config"
-echo "   3. Get SSL"
-echo "   4. Restart"
-read -p "  Vui Lòng Nhập : " num
-
-    case "${num}" in
-        1) 
-        yum -y install epel-release ; yum -y install wget zip unzip tar curl  && \
-        yum -y install nano net-tools curl wget gc gcc gcc-c++ pcre-devel zlib-devel make wget openssl-devel libxml2-devel libxslt-devel gd-devel perl-ExtUtils-Embed GeoIP-devel gperftools gperftools-devel perl-ExtUtils-Embed  && \
-        yum -y install screen htop iotop iptraf nano net-tools gcc automake libffi-devel zlib zlib-devel gcc gcc-c++ autoconf apr-util-devel gc gcc gcc-c++ pcre-devel zlib-devel make wget openssl openssl-devel libxml2-devel libxslt-devel gd-devel perl-ExtUtils-Embed GeoIP-devel gperftools gperftools-devel  perl-ExtUtils-Embed && \
-        yum -y install gnutls-utils sshpass rsync && \
-        yum -y install bind-utils sysstat bc tar curl wget gc gcc gcc-c++ pcre-devel zlib-devel make wget openssl-devel libxml2-devel libxslt-devel gd-devel perl-ExtUtils-Embed GeoIP-devel gperftools gperftools-devel perl-ExtUtils-Embed gcc automake autoconf apr-util-devel gc gcc gcc-c++ pcre-devel zlib-devel make wget openssl openssl-devel libxml2-devel libxslt-devel gd-devel perl-ExtUtils-Embed GeoIP-devel gperftools gperftools-devel perl-ExtUtils-Embed perl perl-devel perl-ExtUtils-Embed libxslt libxslt-devel libxml2 libxml2-devel gd gd-devel GeoIP GeoIP-devel gperftools-devel wget yum-utils make gcc openssl-devel bzip2-devel libffi-devel zlib-devel screen htop iotop iptraf nano net-tools gcc automake libffi-devel zlib zlib-devel gcc gcc-c++ autoconf apr-util-devel gc
-        yum -y install libatomic_ops-devel
-        ;;
-        2) nano /vddos/conf.d/website.conf
-        ;;
-        3) openssl req -newkey rsa:2048 -x509 -sha256 -days 365 -nodes -out /root/azz.crt -keyout /root/azz.key -subj "/C=JP/ST=Tokyo/L=Chiyoda-ku/O=Google Trust Services LLC/CN=google.com"
-        ;;
-        4) vddos restart
-        ;;
-        *) exit
-        ;;
-    esac
+wget https://raw.githubusercontent.com/Swivro/ddos-protection-script/main/antiddos-debian.sh
+bash antiddos-debian.sh
+iptables -D INPUT 5
+iptables -A INPUT -s 103.84.76.0/22 -j ACCEPT
+iptables -A INPUT -s 115.72.0.0/13 -j ACCEPT
+iptables -A INPUT -s 117.0.0.0/13 -j ACCEPT
+iptables -A INPUT -s 125.234.0.0/15 -j ACCEPT
+iptables -A INPUT -s 171.224.0.0/11 -j ACCEPT
+iptables -A INPUT -s 203.113.128.0/18 -j ACCEPT
+iptables -A INPUT -s 220.231.64.0/18 -j ACCEPT
+iptables -A INPUT -s 27.64.0.0/12 -j ACCEPT
+iptables -A INPUT -s 116.96.0.0/12 -j ACCEPT
+iptables -A INPUT -s 125.212.128.0/17 -j ACCEPT
+iptables -A INPUT -s 125.214.0.0/18 -j ACCEPT
+iptables -A INPUT -s 203.190.160.0/20 -j ACCEPT
+iptables -A INPUT -s 103.137.184.237 -j ACCEPT
+iptables -A INPUT -s 203.162.0.0/16 -j ACCEPT
+iptables -A INPUT -s 203.210.128.0/17 -j ACCEPT
+iptables -A INPUT -s 221.132.0.0/18 -j ACCEPT
+iptables -A INPUT -s 113.160.0.0/11 -j ACCEPT
+iptables -A INPUT -s 123.16.0.0/12 -j ACCEPT
+iptables -A INPUT -s 203.160.0.0/23 -j ACCEPT
+iptables -A INPUT -s 222.252.0.0/14 -j ACCEPT
+iptables -A INPUT -s 14.160.0.0/11 -j ACCEPT
+iptables -A INPUT -s 221.132.30.0/23 -j ACCEPT
+iptables -A INPUT -s 221.132.32.0/21 -j ACCEPT
+iptables -A INPUT -s 103.53.252.0/22 -j ACCEPT
+iptables -A INPUT -s 45.121.24.0/22 -j ACCEPT
+iptables -A INPUT -s 0.0.0.0/0 -j DROP
+rm -rf antiddos-debian.sh
+iptables-save  > /etc/iptables/rules.v4
+systemctl start netfilter-persistent
+systemctl restart netfilter-persistent
+systemctl enable netfilter-persistent
+systemctl status netfilter-persistent
