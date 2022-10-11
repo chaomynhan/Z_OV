70e6cfbcff61d813da3084b00c6b8f29d9935d4b
AZZ-vopp <64297334+AZZ-vopp@users.noreply.github.com>
2022-10-12 01:37:43 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 LANG=en_US.UTF-8
@@ -739,6 +740,7 @@ Set_Bt_Panel() {
     auth_path=$(cat ${admin_auth})
     cd ${setup_path}/server/panel/
     if [ "$SET_SSL" == true ]; then
+        mkdir /www/server/panel/ssl
         /www/server/panel/pyenv/bin/pip install -I pyOpenSSl
         /www/server/panel/pyenv/bin/python /www/server/panel/tools.py ssl
     fi
@@ -938,4 +940,4 @@ echo -e "=================================================================="
 endTime=$(date +%s)
 ((outTime = ($endTime - $startTime) / 60))
 echo -e "Time consumed:\033[32m $outTime \033[0mMinute!"
-rm -f install.sh
+rm -f install-ubuntu_6.0_en.sh
