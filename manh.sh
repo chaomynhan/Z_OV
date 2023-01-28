bee2cda0889a10d4dbd4605b0f429f05ed46dbca
AZZ-vopp <64297334+AZZ-vopp@users.noreply.github.com>
2023-01-28 11:35:14 +0700

    Update manh.sh

--- manh.sh
+++ manh.sh
@@ -9,8 +9,7 @@ bash <(curl -ls https://raw.githubusercontent.com/AZZ-vopp/Z_OV/main/xrayrthuong
     read -p "Nhập Api Key :" APIKEY
     echo -e "API KEY là : ${APIKEY}"
 
-    read -p "Nhập Node ID port 80 :" node_443trojan
-    echo -e "node_443trojan là : ${node_443trojan}"
+
 
     read -p "Nhập Node ID port 443 :" node_443vmess
     echo -e "Node_443vmess là : ${node_443vmess}"    
@@ -20,9 +19,7 @@ bash <(curl -ls https://raw.githubusercontent.com/AZZ-vopp/Z_OV/main/xrayrthuong
 
     read -p "Nhập DeviceLimit :" DeviceLimit
     echo -e "DeviceLimit = ${DeviceLimit}"
-
-    read -p "Nhập dns trên cloudflare node 443 trojan:" CertDomain443trojan
-    echo -e "CertDomain443trojan = ${CertDomain443trojan}"       
+    
     
     read -p "Nhập dns trên cloudflare node 443 vmesss:" CertDomain443vmess
     echo -e "CertDomain443vmess = ${CertDomain443vmess}"    
@@ -46,47 +43,6 @@ ConnetionConfig:
   DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
   BufferSize: 64 # The internal cache size of each connection, kB 
 Nodes:
-  -
-    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
-    ApiConfig:
-      ApiHost: "$domain"
-      ApiKey: "$APIKEY"
-      NodeID: $node_443trojan
-      NodeType: Trojan # Node type: V2ray, Trojan, Shadowsocks, Shadowsocks-Plugin
-      Timeout: 30 # Timeout for the api request
-      EnableVless: false # Enable Vless for V2ray Type
-      EnableXTLS: false # Enable XTLS for V2ray and Trojan
-      SpeedLimit: $SpeedLimit # Mbps, Local settings will replace remote settings, 0 means disable
-      DeviceLimit: $DeviceLimit # Local settings will replace remote settings, 0 means disable
-      RuleListPath: /etc/XrayR/AikoBlock # /etc/XrayR/rulelist Path to local rulelist file
-    ControllerConfig:
-      ListenIP: 0.0.0.0 # IP address you want to listen
-      SendIP: 0.0.0.0 # IP address you want to send pacakage
-      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
-      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
-      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
-      DisableUploadTraffic: false # Disable Upload Traffic to the panel
-      DisableGetRule: false # Disable Get Rule from the panel
-      DisableIVCheck: false # Disable the anti-reply protection for Shadowsocks
-      DisableSniffing: true # Disable domain sniffing 
-      EnableProxyProtocol: false # Only works for WebSocket and TCP
-      EnableFallback: false # Only support for Trojan and Vless
-      FallBackConfigs:  # Support multiple fallbacks
-        -
-          SNI: # TLS SNI(Server Name Indication), Empty for any
-          Path: # HTTP PATH, Empty for any
-          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
-          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
-      CertConfig:
-        CertMode: dns # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
-        CertDomain: "$CertDomain443trojan" # Domain to cert
-        CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file
-        KeyFile: /etc/XrayR/cert/node1.test.com.key
-        Provider: cloudflare # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
-        Email: test@me.com
-        DNSEnv: # DNS ENV option used by DNS provider
-          CLOUDFLARE_EMAIL: haihoangnguyen207@gmail.com
-          CLOUDFLARE_API_KEY: 027d267e0f061e57d9b3944b683b567a9dba6
   -
     PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
     ApiConfig:
